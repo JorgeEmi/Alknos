c9d10cb48472aa86b7cf519cf2c653d38f9c50ba
JorgeEmi <mendoza.bautista.jorge.emilio@gmail.com>
2022-11-24 04:50:06 -0600

    Agregué cambios a mi repo4!

--- vite-project/src/CompuestoM.jsx
+++ vite-project/src/CompuestoM.jsx
@@ -1,8 +1,48 @@
 
 const CompuestoM = () => (
     <div className="bg-primary w-full overflow-hidden">
-      
-
+      <div className="px-4 py-5 sm:px-6">
+        <h3 className="text-lg leading-6 font-medium text-green-400">Información del compuesto</h3>
+        <p className="mt-1 max-w-2xl text-sm text-green-500">Información acerca del compuesto.</p>
+      </div>
+      <div className="border-t border-gray-200">
+        <dl>
+          <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <dt className="text-sm font-medium text-gray-500">Nombre del compuesto</dt>
+            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+            <input
+                        id="RESPUESTACOMPUESTO"
+                        name="RESPUESTACOMPUESTO"
+                        type="text"
+                        required
+                        className=" appearance-none block w-full px-5 py-6 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm"
+                      />
+            </dd>
+          </div>
+          <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <dt className="text-sm font-medium text-gray-500">Modelo en 3d renderizado</dt>
+            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+            <button
+                    type="submit"
+                    className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
+                  >
+                    Obtener modelo
+                  </button>
+            </dd>
+          </div>
+          <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <dt className="text-sm font-medium text-gray-500">Compuesto balanceado</dt>
+            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+            <button
+                    type="submit"
+                    className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
+                  >
+                    Obtener balanceo
+                  </button>
+            </dd>
+          </div>
+        </dl>
+      </div>
     </div>
   );
 
